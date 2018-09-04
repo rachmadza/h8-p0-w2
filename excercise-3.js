348aebfc30696ded47241963c5850c0eb24cae2c
rachmadza <razato.zai@gmail.com>
2018-09-04 19:18:22 +0700

    update jawaban soal tugas 3

--- excercise-3.js
+++ excercise-3.js
@@ -1,30 +1,33 @@
-var nama = 'Zero';
-var peran = 'Penyihir';
+var nama = '0';
+var peran = 'Tabib' ;
 
-if (nama == '' && peran == ''){
-    console.log('Nama harus diisi!');
-}
-else if (nama !== '' && peran ==''){
-    console.log('Halo ' +nama+ ', Pilih peranmu untuk memulai game!' );
-}
+if (!nama){
+    console.log('Nama harus diisi!')
+} 
 else {
-    console.log('Selamat datang di Dunia Proxytia, ' +nama);
-    switch (peran) {
-        case 'Ksatria': {
-            console.log('Halo ' +peran , nama+ ', kamu dapat menyerang dengan senjatamu!');
-            break;
-        }
-        case 'Tabib': {
-            console.log('Halo ' +peran , nama+ ', kamu akan membantu temanmu yang terluka!');
-            break;
-        }
-        case 'Penyihir': {
-            console.log('Halo ' +peran , nama+ ', ciptakan keajaiban yang membantu kemenanganmu!');
-            break;
-        }
-        default: {
-            console.log('Silahkan pilih peranmu untuk mulai berpetualang');
-        }
-        
+   //nama ada isinya 
+    if (!peran){
+        console.log('Halo ' + nama + ', Pilih peranmu untuk memulai game!')
     }
+    else {
+        console.log('Selamat datang di Dunia Proxytia, ' +nama)
+        if (peran == 'Ksatria') {
+            //console.log('Halo Ksatria '+nama+ ', kamu dapat menyerang dengan senjatamu!')
+            console.log('Halo ' +peran+ ' ' +nama+ ', kamu dapat menyerang dengan senjatamu!')
+        }
+        else if (peran == 'Tabib') {
+            //console.log('Halo Tabib '+nama+ ', kamu akan membantu temanmu yang terluka.')
+            console.log('Halo ' +peran+ ' ' +nama+ ', kamu akan membantu temanmu yang terluka.')
+        }
+        else if (peran == 'Penyihir') {
+            //console.log('Halo Penyihir '+nama+ ', ciptakan keajaiban yang membantu kemenanganmu!')
+            console.log('Halo ' +peran+ ' ' +nama+ ', ciptakan keajaiban yang membantu kemenanganmu!')
+        }
+        else {
+            console.log('Silahkan pilih peran yang sesuai');
+        }
+    } 
 }
+
+
+
